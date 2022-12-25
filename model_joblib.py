708479923dca23719121c6d1dcd50a674f7f10a3
Md. Taufiqul Haque Khan Tusar <taufiqkhantusar@gmail.com>
2022-12-25 22:48:49 +0600

    missing value in form is now replaced by np..NaN

--- model_joblib.py
+++ model_joblib.py
@@ -1,4 +1,4 @@
-                     # Dependencies list to execute programs
+# Dependencies list to execute programs
 
 #Data manipulation
 import numpy as np
@@ -7,21 +7,35 @@ from trainingRFmodel import *
 import joblib
 RF8 = joblib.load("RF8")
 
+
 def CKD_Prediction(specific_gravety, albumin, blood_urea, serum_creatinine, hemoglobine, red_blood_cells, hypertension, diabetes_mellitias):
-   
     """PREDICTION"""
-    
+
     #Using Numpy Array
     Xtest = np.array([[specific_gravety, albumin, blood_urea, serum_creatinine, hemoglobine, red_blood_cells, hypertension, diabetes_mellitias]])
-    print(f"Xtest: {Xtest}")
+    # print(f"Xtest: {Xtest}")
+    
     # # Using List
     # Xtest = [[specific_gravety, albumin, blood_urea, serum_creatinine, hemoglobine, red_blood_cells, hypertension, diabetes_mellitias]]
 
+    new_Xtest = []
+    for row in Xtest:
+        for element in row:
+            if element == '':
+                element = np.NaN
+                new_Xtest.append(element)
+            else: 
+                element.astype(float)
+                new_Xtest.append(element)
+    Xtest =  np.array([new_Xtest])
+    # print(f"Xtest: {Xtest}")
+
+
     Xtest = normalizer.transform(Xtest)
-    print(f"Scalled result: {Xtest}")
+    # print(f"Scalled result: {Xtest}")
     Xtest = imputer.transform(Xtest) #Appling Imputation
-    print(f"Impute result: {Xtest}")
-    Xtest = standardization.transform(Xtest)
+    # print(f"Impute result: {Xtest}")
+    # Xtest = standardization.transform(Xtest)
     result = RF8.predict(Xtest)
     for i in result:  
         r = (i)
