2d586b1d1e596cddd4b0d7f89b1397380aa7574c
Md. Taufiqul Haque Khan Tusar <taufiqkhantusar@gmail.com>
2022-12-25 20:40:01 +0600

    test branch

--- model_joblib.py
+++ model_joblib.py
@@ -13,12 +13,14 @@ def CKD_Prediction(specific_gravety, albumin, blood_urea, serum_creatinine, hemo
     
     #Using Numpy Array
     Xtest = np.array([[specific_gravety, albumin, blood_urea, serum_creatinine, hemoglobine, red_blood_cells, hypertension, diabetes_mellitias]])
-
+    print(f"Xtest: {Xtest}")
     # # Using List
     # Xtest = [[specific_gravety, albumin, blood_urea, serum_creatinine, hemoglobine, red_blood_cells, hypertension, diabetes_mellitias]]
 
     Xtest = normalizer.transform(Xtest)
+    print(f"Scalled result: {Xtest}")
     Xtest = imputer.transform(Xtest) #Appling Imputation
+    print(f"Impute result: {Xtest}")
     Xtest = standardization.transform(Xtest)
     result = RF8.predict(Xtest)
     for i in result:  
